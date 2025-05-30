a56f3d8195f1b641acf585cb586be40cf5badb05
John Clements <john.clements@my.avemaria.edu>
2025-05-30 10:10:31 -0500

    Updated the javascript to be able to target each miracle card and its elements by id. Still need to figure out how to do expanded view.

--- script.js
+++ script.js
@@ -8,17 +8,56 @@ async function showMiracles() {
         miracles.forEach(miracle => {
             let div = document.createElement('div');
             div.innerHTML = '';
-            div.innerHTML += `<h2 class="card-title">${miracle.title}</h2>`;
-            div.innerHTML += `<h3 class="card-location">${miracle.location}</h3>`;
-            div.innerHTML += `<h3 class="card-year">${miracle.year}</h3>`
-            div.innerHTML += `<p class="card-summary">${miracle.summary}</p>`;
-            div.innerHTML += `<button class="card-button">Learn More</button>`;
             div.className = 'miracle-card';
             card_grid.appendChild(div);
+            div.id = 'card' + miracle.id;
+            // ChatGPT utilized for debugging in order to properly append my created h2 element. I was forgetting to
+            // say appendChild() and was instead just updating the innerHTML. Cite ChatGPT here.
+            let title = document.createElement('h2');
+            title.innerHTML = '';
+            title.textContent = miracle.title;
+            title.className = 'card-title';
+            let title_id = 'title' + miracle.id;
+            title.id = title_id;
+            div.appendChild(title);
+            let location_id = 'location' + miracle.id;
+            let location = document.createElement('h3');
+            location.textContent = miracle.location;
+            location.className = 'card-location';
+            location.id = location_id;
+            div.appendChild(location);
+            let year_id = 'year' + miracle.id;
+            let year = document.createElement('h3');
+            year.innerHTML = '';
+            year.textContent = miracle.year;
+            year.className = 'card-year';
+            year.id = year_id;
+            div.appendChild(year);
+            let summary_id = 'summary' + miracle.id;
+            let summary = document.createElement('p');
+            summary.innerHTML = '';
+            summary.textContent = miracle.summary;
+            summary.className = 'card-summary';
+            summary.id = summary_id;
+            div.appendChild(summary);
+            let button_id = 'button' + miracle.id;
+            let button = document.createElement('button');
+            button.innerHTML = '';
+            button.textContent = 'Learn More';
+            button.className = 'card-button';
+            button.id= button_id;
+            div.appendChild(button);
         })
+        for (let i = 1; i <= miracles.length; i++) {
+            let cardButton = document.getElementById('button' + i);
+            cardButton.addEventListener('click', event => {
+            })
+        }
     } catch(error) {
         console.log('Fetch error:', error);
     }
 }
+
+
 
 showMiracles();
