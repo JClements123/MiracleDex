a9079afbe73fc232be10edbf6473a598e319b76d
John Clements <john.clements@my.avemaria.edu>
2025-05-30 15:23:06 -0500

    Added some CSS to make the cards shift when hovered. Updated the JavaScript to include a load more button that will allow the user to load 6 more miracles at a time. The button will disappear when all of the miracles in the database are loaded, and no extra cards will be formed.

--- script.js
+++ script.js
@@ -5,48 +5,101 @@ async function showMiracles() {
         const miracles = await response.json();
         console.log(miracles);
         const card_grid = document.getElementById('card-grid');
-        miracles.forEach(miracle => {
+        for (let i = 0; i < 6; i++) {
+        // miracles.forEach(miracle => {
             let div = document.createElement('div');
             div.innerHTML = '';
             div.className = 'miracle-card';
             card_grid.appendChild(div);
-            div.id = 'card' + miracle.id;
+            div.id = 'card' + miracles[i].id;
             // ChatGPT utilized for debugging in order to properly append my created h2 element. I was forgetting to
             // say appendChild() and was instead just updating the innerHTML. Cite ChatGPT here.
             let title = document.createElement('h2');
             title.innerHTML = '';
-            title.textContent = miracle.title;
+            title.textContent = miracles[i].title;
             title.className = 'card-title';
-            let title_id = 'title' + miracle.id;
+            let title_id = 'title' + miracles[i].id;
             title.id = title_id;
             div.appendChild(title);
-            let location_id = 'location' + miracle.id;
+            let location_id = 'location' + miracles[i].id;
             let location = document.createElement('h3');
-            location.textContent = miracle.location;
+            location.textContent = miracles.location;
             location.className = 'card-location';
             location.id = location_id;
             div.appendChild(location);
-            let year_id = 'year' + miracle.id;
+            let year_id = 'year' + miracles[i].id;
             let year = document.createElement('h3');
             year.innerHTML = '';
-            year.textContent = miracle.year;
+            year.textContent = miracles[i].year;
             year.className = 'card-year';
             year.id = year_id;
             div.appendChild(year);
-            let summary_id = 'summary' + miracle.id;
+            let summary_id = 'summary' + miracles[i].id;
             let summary = document.createElement('p');
             summary.innerHTML = '';
-            summary.textContent = miracle.summary;
+            summary.textContent = miracles[i].summary;
             summary.className = 'card-summary';
             summary.id = summary_id;
             div.appendChild(summary);
-            let button_id = 'button' + miracle.id;
+            let button_id = 'button' + miracles[i].id;
             let button = document.createElement('button');
             button.innerHTML = '';
             button.textContent = 'Learn More';
             button.className = 'card-button';
             button.id= button_id;
             div.appendChild(button);
+        }
+        const loadMore = document.getElementById('load-more');
+        loadMore.addEventListener('click', () => {
+            let count = card_grid.children.length;
+            for (let i = count; i < (count + 6); i++) {
+                if (i >= miracles.length) {
+                    const loadButton = document.getElementById('load-more');
+                    loadButton.remove();
+                    break;
+                }
+                let div = document.createElement('div');
+                div.innerHTML = '';
+                div.className = 'miracle-card';
+                card_grid.appendChild(div);
+                div.id = 'card' + miracles[i].id;
+                // ChatGPT utilized for debugging in order to properly append my created h2 element. I was forgetting to
+                // say appendChild() and was instead just updating the innerHTML. Cite ChatGPT here.
+                let title = document.createElement('h2');
+                title.innerHTML = '';
+                title.textContent = miracles[i].title;
+                title.className = 'card-title';
+                let title_id = 'title' + miracles[i].id;
+                title.id = title_id;
+                div.appendChild(title);
+                let location_id = 'location' + miracles[i].id;
+                let location = document.createElement('h3');
+                location.textContent = miracles.location;
+                location.className = 'card-location';
+                location.id = location_id;
+                div.appendChild(location);
+                let year_id = 'year' + miracles[i].id;
+                let year = document.createElement('h3');
+                year.innerHTML = '';
+                year.textContent = miracles[i].year;
+                year.className = 'card-year';
+                year.id = year_id;
+                div.appendChild(year);
+                let summary_id = 'summary' + miracles[i].id;
+                let summary = document.createElement('p');
+                summary.innerHTML = '';
+                summary.textContent = miracles[i].summary;
+                summary.className = 'card-summary';
+                summary.id = summary_id;
+                div.appendChild(summary);
+                let button_id = 'button' + miracles[i].id;
+                let button = document.createElement('button');
+                button.innerHTML = '';
+                button.textContent = 'Learn More';
+                button.className = 'card-button';
+                button.id= button_id;
+                div.appendChild(button);
+            }
         })
         for (let i = 1; i <= miracles.length; i++) {
             let cardButton = document.getElementById('button' + i);
@@ -65,6 +118,9 @@ async function showMiracles() {
         console.log('Fetch error:', error);
     }
 }
+
+
+
 
 console.log('Hello World');
 
